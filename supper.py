6211abdf07764bbd3cc085382d89bb3b2593265d
oycq <490746960@qq.com>
2019-09-05 21:04:31 +0800

    thanks god

--- supper.py
+++ supper.py
@@ -5,6 +5,7 @@ import torch.nn.init
 import torch.optim as optim
 import random
 import os
+import time
 
 lstm_width = 50
 input_width = 2
@@ -12,13 +13,14 @@ internal_width = 5
 output_width = 2
 pre_length = 15
 suf_length = 5
-cuda = 0 
+cuda = 1
 control_optimized_times = 20
 database_size = 100000
-ai_affluence = 10
+ai_affluence = 1
 internal_params_name = {'time':0, 'object':1, 
                         'imu_speed_x':2, 'imu_speed_y':3, 'imu_speed':4}
-train_batch_size = 50
+train_batch_size = 2000
+old_history_ratio = 0.9
 optimize_loss_k = 0.1
 
 
@@ -36,13 +38,13 @@ class AiController():
         l = self.ai.train(a['prior'], a['input'], a['output'])
         return l
         
-    def get_optimized_control(self, previous): #return c == next_control
+    def get_optimized_control(self):
         a = self.database.get_current_prior_info()
         if a is None:
             print('Need preheat..')
             os._exit(0)
-        c = self.ai.train(a)
-        return c[0]
+        c = self.ai.get_optimized_control(a)
+        return c
 
     def append_internal_data(self,data):
         self.database.append_internal_data(data)
@@ -61,9 +63,9 @@ class AiController():
             self.output_array = np.zeros((database_size, output_width), dtype ='float32')
             self.merged_array = np.zeros((database_size,input_width + internal_width + output_width), dtype ='float32')
             self.array_pointer = -1
-            self.input_batch = np.zeros((database_size, suf_length, input_width))
-            self.prior_batch = np.zeros((database_size, pre_length, input_width + internal_width + output_width))
-            self.output_batch = np.zeros((database_size, suf_length, output_width))
+            self.input_batch = np.zeros((database_size, suf_length, input_width), dtype ='float32')
+            self.prior_batch = np.zeros((database_size, pre_length, input_width + internal_width + output_width), dtype ='float32')
+            self.output_batch = np.zeros((database_size, suf_length, output_width), dtype ='float32')
             self.batch_pointer = 0
 
 
@@ -100,12 +102,15 @@ class AiController():
         def get_train_batch(self):
             if self.array_pointer == 0:
                 return None
-            if self.batch_pointer > train_batch_size * 2:
-                index = random.sample(range(self.batch_pointer - train_batch_size), train_batch_size)
-                index += [self.batch_pointer - train_batch_size + x for x in range(train_batch_size)]
+            if self.batch_pointer > train_batch_size:
+                index = random.sample(range(self.batch_pointer - int(train_batch_size * (1 - old_history_ratio))), int(train_batch_size * old_history_ratio))
+                index += [int(self.batch_pointer - train_batch_size * (1 - old_history_ratio) + x) for x in range(int(train_batch_size - train_batch_size * old_history_ratio))]
             else:
                 index = range(self.batch_pointer)
-            batch_data = {'prior':self.prior_batch[index], 'input':self.input_batch[index], 'output':self.output_batch[index]}
+            if cuda == 0: 
+                batch_data = {'prior':torch.as_tensor(self.prior_batch[index]), 'input':torch.as_tensor(self.input_batch[index]), 'output':torch.as_tensor(self.output_batch[index])}
+            else:
+                batch_data = {'prior':torch.as_tensor(self.prior_batch[index]).cuda(), 'input':torch.as_tensor(self.input_batch[index]).cuda(), 'output':torch.as_tensor(self.output_batch[index]).cuda()}
             return batch_data
 
         def get_current_prior_info(self):
@@ -113,6 +118,10 @@ class AiController():
                 return None
             data = self.merged_array[self.array_pointer - pre_length : self.array_pointer]
             data = data.reshape((1, pre_length, internal_width + input_width + output_width))
+            if cuda == 0:
+                data = torch.as_tensor(data)
+            else:
+                data = torch.as_tensor(data).cuda()
             return data
 
 
@@ -128,7 +137,7 @@ class AiController():
                 super().__init__()
                 w = input_width + internal_width + output_width
                 self.lstm_a = nn.LSTM(w, lstm_width, num_layers = 1, batch_first = True)
-                self.lstm_b = nn.LSTM(w, lstm_width, num_layers = 1, batch_first = True)
+                self.lstm_b = nn.LSTM(input_width, lstm_width, num_layers = 1, batch_first = True)
                 self.linear_a = nn.Linear(lstm_width, output_width)
                 self.linear_b = nn.Linear(lstm_width, output_width * suf_length)
                 self._init_params(self.lstm_a)
@@ -151,33 +160,36 @@ class AiController():
             def forward(self, prior_batch, input_batch = None, output_batch = None):
                 if input_batch is not None:
                     a, (h_n, c_n) = self.lstm_a(prior_batch)
-                    b, _ = self.lstm_b(control_batch, (h_n, c_n))
-                    predict = self.linear_a(b)[:,:,0,:]
-                    predict_loss = torch.mean((input_batch - output_batch).abs())
+                    b, _ = self.lstm_b(input_batch, (h_n, c_n))
+                    predict = self.linear_a(b)
+                    predict_loss = torch.mean((predict- output_batch).abs())
 
                     self.lstm_a.requires_grad = False
                     self.lstm_b.requires_grad = False
                     self.linear_a.requires_grad = False 
                     a, (h_n, c_n) = self.lstm_a(prior_batch)
                     b = self.linear_b(c_n[0])
-                    b = torch.nn.functional.tanh(b) * ai_affluence
-                    optimize_control = b.view(-1, output_width, suf_length)
+#                    b = torch.tanh(b) * ai_affluence
+                    optimize_control = b.view(-1, suf_length, output_width)#.fill_(0.5)
                     c, _ = self.lstm_b(optimize_control, (h_n, c_n))
-                    optimize_result = self.linear_a(c)[:,:,0,:]
-                    self.linear_a.requires_grad =  optimize_loss = torch.mean(optimize_result.abs())
+                    optimize_result = self.linear_a(c)
+                    optimize_loss = torch.mean(optimize_result.abs())
                     self.lstm_a.requires_grad = True
                     self.lstm_b.requires_grad = True
                     self.linear_a.requires_grad = True 
 
-                    loss = predict_loss - optimize_loss * optimize_loss_k
+                    loss = predict_loss + optimize_loss * optimize_loss_k
+                    #print("%10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f %10.4f"%(input_batch[-1][0][0],optimize_control[-1][0][0],optimize_result[-1][0][0], predict[-1][0][0], output_batch[-1][0][0],predict_loss, optimize_loss, loss))
+                    print("%10.4f %10.4f %10.4f %10.4f %10.4f"%(input_batch[-1][0][0],optimize_control[-1][0][0], predict_loss, optimize_loss, loss))
                     return loss, predict_loss, optimize_loss
 
                 else:
                     a, (h_n, c_n) = self.lstm_a(prior_batch)
                     b = self.linear_b(c_n[0]) 
-                    b = torch.nn.functional.tanh(b) * ai_affluence
-                    optimize_control = b.view(-1, output_width, suf_length)
-                    return optimize_control.numpy()
+#                    b = torch.tanh(b) * ai_affluence
+                   # print(b)
+                    optimize_control = b.view(-1, suf_length, output_width)[0][0]
+                    return optimize_control.detach().cpu().numpy()
                 
         def train(self, prior_batch, input_batch, output_batch):
             loss, predict_loss, optimize_loss = self.model(prior_batch,input_batch,output_batch)
--- t.py
+++ t.py
@@ -1,17 +1,33 @@
+import time
 import numpy as np
 import supper
-
+import random
 controller = supper.AiController()
-for i in range(150):
-    internal_data = np.array([i,1,i,i,i])
-    output_data = np.array([i,i])
+
+j = random.gauss(0,1)
+input_data = np.array([0.25,0.25])
+for i in range(15000):
+#    j = (i % 200 - 100) / 100# + random.gauss(0,0.002)
+    internal_data = np.array([0,1,0,0,0])
+    z = input_data[0]
+    z = (z*z/2 - 2)*(z-1)
+    output_data = [z,z]
     controller.append_internal_data(internal_data)
     controller.append_output_data(output_data)
-    if i == 21:
-#       print(controller.database.get_current_batch()['prior'][-1])
-        print(controller.database.get_current_prior_info())
-        break
-    input_data = np.array([i,i])
+#    if i == 22:
+#        print(controller.database.get_train_batch()['prior'])
+#        print(controller.database.get_train_batch()['input'])
+#        print(controller.database.get_train_batch()['output'])
+#        print(controller.database.get_current_prior_info())
+
+    asd = time.time() * 1000
+    j = random.gauss(0,1)
+    input_data = np.array([j,j])
+    if i  >= 21:
+        controller.train()
+    #    print(time.time()*1000-asd)
+        a = controller.get_optimized_control()
+    #    print(time.time()*1000-asd)
+        input_data = a
     controller.append_input_data(input_data)
-
-
+    #print(time.time()*1000-asd)
